fc3c64abd4ee060b99767c290acfe95b104947dc
david-siegert <siegert.D@seznam.cz>
2019-10-07 23:24:30 +0200

    .

--- Astrometrie/Properties/Resources.Designer.cs
+++ Astrometrie/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Astrometrie.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap blue_skies_on_pluto_final_2 {
-            get {
-                object obj = ResourceManager.GetObject("blue_skies_on_pluto_final_2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
